7e2d0db6698a2c6050daa29ebee809e86837db58
Dan C <dcro202@hotmail.com>
2019-09-13 13:04:33 -0500

    Added parentLayout and fixed the setText issue

--- app/src/main/java/com/example/android/recipeapp/RecipeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/recipeapp/RecipeRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.example.android.recipeapp;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,16 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.android.recipeapp.data.Recipe;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecyclerViewAdapter.ViewHolder> {
 
     private static final String LOG_TAG = RecipeRecyclerViewAdapter.class.getSimpleName();
 
-    private ArrayList<Recipe> mRecipeDetails;
+    private List<Recipe> mRecipeDetails;
     private Activity mContext;
 
-    public RecipeRecyclerViewAdapter(Activity mContext, ArrayList<Recipe> mRecipeDetails) {
+    public RecipeRecyclerViewAdapter(Activity mContext, List<Recipe> mRecipeDetails) {
         this.mContext = mContext;
         this.mRecipeDetails = mRecipeDetails;
     }
@@ -29,19 +32,20 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
 
     @NonNull
     @Override
-    public RecipeRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_recipes, viewGroup, false);
-        RecipeRecyclerViewAdapter.ViewHolder viewHolder = new RecipeRecyclerViewAdapter.ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(RecipeRecyclerViewAdapter.ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
+        Log.d(LOG_TAG, "onBindViewHolder called.");
         final Recipe recipeDetails = mRecipeDetails.get(position);
 
-        viewHolder.recipeName.setText(recipeDetails.getRecipeName());
-        viewHolder.servingSize.setText(recipeDetails.getServingSize());
+        viewHolder.recipeName.setText(String.valueOf(recipeDetails.getRecipeName()));
+        viewHolder.servingSize.setText(String.valueOf(recipeDetails.getServingSize()));
 
     }
 
@@ -54,7 +58,7 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
         }
     }
 
-    public void updateRecipeList(ArrayList<Recipe> recipeContent) {
+    public void updateRecipeList(List<Recipe> recipeContent) {
         this.mRecipeDetails = recipeContent;
         notifyDataSetChanged();
     }
@@ -64,12 +68,14 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
 
         TextView recipeName;
         TextView servingSize;
+        LinearLayout parentLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             recipeName = itemView.findViewById(R.id.recipe_name);
             servingSize = itemView.findViewById(R.id.serving_size);
+            parentLayout = itemView.findViewById(R.id.recipe_list_layout);
 
         }
     }
