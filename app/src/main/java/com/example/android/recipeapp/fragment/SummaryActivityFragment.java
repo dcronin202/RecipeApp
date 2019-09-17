7b80298de47eb300fff23bab92a0d24721e90272
Dan C <dcronin202@gmail.com>
2019-09-17 09:57:27 -0500

    Updated content in populateRecipeDetails method

--- app/src/main/java/com/example/android/recipeapp/fragment/SummaryActivityFragment.java
+++ app/src/main/java/com/example/android/recipeapp/fragment/SummaryActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.recipeapp.fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.android.recipeapp.R;
 import com.example.android.recipeapp.data.Recipe;
+import com.example.android.recipeapp.data.RecipeStep;
 import com.example.android.recipeapp.viewmodel.RecipeViewModel;
 
 public class SummaryActivityFragment extends Fragment {
@@ -27,7 +29,7 @@ public class SummaryActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_recipe_overview, container, false);
+        View view = inflater.inflate(R.layout.fragment_summary, container, false);
 
         viewModel = ViewModelProviders.of(getActivity()).get(RecipeViewModel.class);
 
@@ -40,11 +42,20 @@ public class SummaryActivityFragment extends Fragment {
     private void populateRecipeDetails(View view) {
 
         final Recipe recipe = viewModel.getRecipeDetails();
+        final RecipeStep step = viewModel.getStepDetails();
+
+        //TextView overviewUrl = view.findViewById(R.id.overview_video_url);
+        //overviewUrl.setText(step.getRecipeVideoUrl());
 
         TextView recipeName = view.findViewById(R.id.overview_recipe_name);
         recipeName.setText(recipe.getRecipeName());
 
-
+        // For showing serving size
+        Resources resources = getResources();
+        String servingSize = String.valueOf(recipe.getServingSize());
+        String servingSizeText = resources.getString(R.string.summary_serving_size, servingSize);
+        TextView servings = view.findViewById(R.id.overview_serving_size);
+        servings.setText(servingSizeText);
     }
 
 }
