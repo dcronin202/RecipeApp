a75e8ec2f569e6a2ea8c75f2c8f16e7ab608e718
Dan C <dcronin202@gmail.com>
2019-09-17 12:58:22 -0500

    Added reyclerview methods for the list of ingredients and steps

--- app/src/main/java/com/example/android/recipeapp/fragment/SummaryActivityFragment.java
+++ app/src/main/java/com/example/android/recipeapp/fragment/SummaryActivityFragment.java
@@ -10,11 +10,19 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.recipeapp.R;
+import com.example.android.recipeapp.adapter.IngredientsRecyclerViewAdapter;
+import com.example.android.recipeapp.adapter.SimpleStepsRecyclerViewAdapter;
+import com.example.android.recipeapp.data.Ingredient;
 import com.example.android.recipeapp.data.Recipe;
 import com.example.android.recipeapp.data.RecipeStep;
+import com.example.android.recipeapp.viewmodel.RecipeDetailViewModel;
 import com.example.android.recipeapp.viewmodel.RecipeViewModel;
+
+import java.util.ArrayList;
 
 public class SummaryActivityFragment extends Fragment {
 
@@ -22,7 +30,11 @@ public class SummaryActivityFragment extends Fragment {
 
     public static final String RECIPE_DETAILS = "recipes";
 
-    private RecipeViewModel viewModel;
+    private RecipeDetailViewModel viewModel;
+
+    private RecyclerView recyclerView;
+    private IngredientsRecyclerViewAdapter ingredientsRecyclerViewAdapter;
+    private SimpleStepsRecyclerViewAdapter simpleStepsRecyclerViewAdapter;
 
 
     @Nullable
@@ -31,9 +43,13 @@ public class SummaryActivityFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_summary, container, false);
 
-        viewModel = ViewModelProviders.of(getActivity()).get(RecipeViewModel.class);
+        viewModel = ViewModelProviders.of(getActivity()).get(RecipeDetailViewModel.class);
 
         populateRecipeDetails(view);
+
+        populateIngredientDetails(view);
+
+        populateRecipeSteps(view);
 
         return view;
 
@@ -42,10 +58,11 @@ public class SummaryActivityFragment extends Fragment {
     private void populateRecipeDetails(View view) {
 
         final Recipe recipe = viewModel.getRecipeDetails();
-        final RecipeStep step = viewModel.getStepDetails();
 
-        //TextView overviewUrl = view.findViewById(R.id.overview_video_url);
-        //overviewUrl.setText(step.getRecipeVideoUrl());
+        TextView overviewUrl = view.findViewById(R.id.overview_video_url);
+        overviewUrl.setText(recipe.getRecipeSteps()
+                .get(recipe.getRecipeSteps().size()-1)
+                .getRecipeVideoUrl());
 
         TextView recipeName = view.findViewById(R.id.overview_recipe_name);
         recipeName.setText(recipe.getRecipeName());
@@ -56,6 +73,30 @@ public class SummaryActivityFragment extends Fragment {
         String servingSizeText = resources.getString(R.string.summary_serving_size, servingSize);
         TextView servings = view.findViewById(R.id.overview_serving_size);
         servings.setText(servingSizeText);
+    }
+
+    private void populateIngredientDetails(View parentView) {
+
+        final Recipe recipe = viewModel.getRecipeDetails();
+
+        recyclerView = parentView.findViewById(R.id.recyclerview_ingredients);
+
+        ingredientsRecyclerViewAdapter = new IngredientsRecyclerViewAdapter(getActivity(), recipe.getRecipeIngredients());
+        recyclerView.setAdapter(ingredientsRecyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    }
+
+    private void populateRecipeSteps(View parentView) {
+
+        final Recipe recipe = viewModel.getRecipeDetails();
+
+        recyclerView = parentView.findViewById(R.id.recyclerview_simple_steps);
+
+        simpleStepsRecyclerViewAdapter = new SimpleStepsRecyclerViewAdapter(getActivity(), recipe.getRecipeSteps());
+        recyclerView.setAdapter(simpleStepsRecyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
     }
 
 }
