6230846d60bc5b4df8c02ba99b418dfa98327be9
Dan C <dcronin202@gmail.com>
2019-09-17 09:46:39 -0500

    Updated layout id names

--- app/src/main/java/com/example/android/recipeapp/SummaryActivity.java
+++ app/src/main/java/com/example/android/recipeapp/SummaryActivity.java
@@ -11,9 +11,9 @@ import com.example.android.recipeapp.data.Recipe;
 import com.example.android.recipeapp.fragment.SummaryActivityFragment;
 import com.example.android.recipeapp.viewmodel.RecipeViewModel;
 
-public class SummmaryActivity extends AppCompatActivity {
+public class SummaryActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = SummmaryActivity.class.getSimpleName();
+    private static final String LOG_TAG = SummaryActivity.class.getSimpleName();
 
     public static final String RECIPE_DETAILS = "recipes";
 
@@ -21,13 +21,13 @@ public class SummmaryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_overview);
+        setContentView(R.layout.activity_summary);
 
         Intent intent = getIntent();
         Recipe recipeDetails = intent.getParcelableExtra(RECIPE_DETAILS);
 
         RecipeViewModel viewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
-        //TODO: Fix this so it isn't returning null
+
         viewModel.setRecipeDetails(recipeDetails);
 
         SummaryActivityFragment overviewFragment = new SummaryActivityFragment();
@@ -35,7 +35,7 @@ public class SummmaryActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         fragmentManager.beginTransaction()
-                .add(R.id.overview_activity, overviewFragment)
+                .replace(R.id.summary_activity_content, overviewFragment)
                 .commit();
     }
 
