3c6e49a5fc98930237d28c2373d7a1ac1b42f72b
Dan C <dcronin202@gmail.com>
2019-10-02 12:07:57 -0500

    Fixed test for checking the video display; added test to check the TextView display, and added the nestedScrollTo method

--- app/src/androidTest/java/com/example/android/recipeapp/RecipeStepsActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/recipeapp/RecipeStepsActivityBasicTest.java
@@ -1,16 +1,23 @@
 package com.example.android.recipeapp;
 
-
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.FrameLayout;
 
+import androidx.core.widget.NestedScrollView;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.util.HumanReadables;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,13 +25,19 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+
 
 @RunWith(AndroidJUnit4.class)
 public class RecipeStepsActivityBasicTest {
@@ -33,7 +46,10 @@ public class RecipeStepsActivityBasicTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void recipeStepVideoPlaybackTest() {
+    public void recipeStepVideoDisplayTest() throws Exception {
+
+        Thread.sleep(1000);
+
         ViewInteraction linearLayout = onView(
                 allOf(withId(R.id.recipe_list_layout),
                 childAtPosition(allOf(withId(R.id.recyclerview_main),
@@ -42,29 +58,44 @@ public class RecipeStepsActivityBasicTest {
 
         linearLayout.perform(click());
 
-        /*
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.button_recipe_steps), withText("Step-by-Step Mode"),
-                childAtPosition(childAtPosition(withClassName(is("androidx.core.widget.NestedScrollView")),
-                0), 6), isDisplayed()));
+        onView(withId(R.id.button_recipe_steps))
+                .perform(nestedScrollTo(), click());
 
-        appCompatButton.perform(click());
-
-        ViewInteraction appCompatImageButton = onView(
+        ViewInteraction recipeStepView = onView(
                 allOf(withId(R.id.exo_play), withContentDescription("Play"),
                 childAtPosition(childAtPosition(withId(R.id.exo_controller),
                 0), 0), isDisplayed()));
 
-        appCompatImageButton.perform(click());
+        recipeStepView.check(matches(isDisplayed()));
 
-        ViewInteraction appCompatImageButton2 = onView(
-                allOf(withId(R.id.exo_pause), withContentDescription("Pause"),
-                childAtPosition(childAtPosition(withId(R.id.exo_controller),
-                0), 1), isDisplayed()));
-
-        appCompatImageButton2.perform(click());
-        */
     }
+
+    @Test
+    public void recipeStepTextDisplayTest() throws Exception {
+
+        Thread.sleep(1000);
+
+        ViewInteraction linearLayout = onView(
+                allOf(withId(R.id.recipe_list_layout),
+                childAtPosition(allOf(withId(R.id.recyclerview_main),
+                childAtPosition(withClassName(is("android.widget.LinearLayout")),
+                0)), 0), isDisplayed()));
+
+        linearLayout.perform(click());
+
+        onView(withId(R.id.button_recipe_steps))
+                .perform(nestedScrollTo(), click());
+
+        ViewInteraction recipeStepView = onView(
+                allOf(withId(R.id.details_step_content), withText("Recipe Introduction"),
+                childAtPosition(allOf(withId(R.id.detailed_steps_layout),
+                withParent(withId(R.id.pager))),
+                0), isDisplayed()));
+
+        recipeStepView.check(matches(isDisplayed()));
+
+    }
+
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
@@ -83,5 +114,67 @@ public class RecipeStepsActivityBasicTest {
                         && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
+    }
+
+    public static ViewAction nestedScrollTo() {
+        return new ViewAction() {
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return Matchers.allOf(
+                        isDescendantOfA(isAssignableFrom(NestedScrollView.class)),
+                        withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE));
+            }
+
+            @Override
+            public String getDescription() {
+                return "View is not NestedScrollView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                try {
+                    NestedScrollView nestedScrollView = (NestedScrollView)
+                            findFirstParentLayoutOfClass(view, NestedScrollView.class);
+                    if (nestedScrollView != null) {
+                        nestedScrollView.scrollTo(0, view.getTop());
+                    } else {
+                        throw new Exception("Unable to find NestedScrollView parent.");
+                    }
+                } catch (Exception e) {
+                    throw new PerformException.Builder()
+                            .withActionDescription(this.getDescription())
+                            .withViewDescription(HumanReadables.describe(view))
+                            .withCause(e)
+                            .build();
+                }
+                uiController.loopMainThreadUntilIdle();
+            }
+
+        };
+    }
+
+    private static View findFirstParentLayoutOfClass(View view, Class<? extends View> parentClass) {
+        ViewParent parent = new FrameLayout(view.getContext());
+        ViewParent incrementView = null;
+        int i = 0;
+        while (parent != null && !(parent.getClass() == parentClass)) {
+            if (i == 0) {
+                parent = findParent(view);
+            } else {
+                parent = findParent(incrementView);
+            }
+            incrementView = parent;
+            i++;
+        }
+        return (View) parent;
+    }
+
+    private static ViewParent findParent(View view) {
+        return view.getParent();
+    }
+
+    private static ViewParent findParent(ViewParent view) {
+        return view.getParent();
     }
 }
