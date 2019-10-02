6004752f2c86dbaf6ec17e5b74f6be3ca00a2d13
Dan C <dcronin202@gmail.com>
2019-10-02 15:23:28 -0500

    Added getRecipeGridRemoteView method to the WidgetProvider

--- app/src/main/java/com/example/android/recipeapp/RecipeWidgetProvider.java
+++ app/src/main/java/com/example/android/recipeapp/RecipeWidgetProvider.java
@@ -6,26 +6,29 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class RecipeWidgetProvider extends AppWidgetProvider {
 
+    public static final String EXTRA_ITEM = "extra_item";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_title);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
-        //views.setTextViewText(R.id.appwidget_recipe_name, widgetText);
+        RemoteViews views;
 
-        // Intent to launch MainActivity when clicked
+        /* Intent to launch MainActivity when clicked
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
         // Launch pending intent
-        views.setOnClickPendingIntent(R.id.appwidget_recipe_name, pendingIntent);
+        views.setOnClickPendingIntent(R.id.appwidget_recipe_name, pendingIntent);*/
+
+        views = getRecipeGridRemoteView(context);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -47,6 +50,24 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    private static RemoteViews getRecipeGridRemoteView(Context context) {
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_grid_view);
+
+        // Set the GridWidgetService to act as the adapter for the GridView
+        Intent intent = new Intent(context, GridWidgetService.class);
+        remoteViews.setRemoteAdapter(R.id.widget_grid_view, intent);
+
+        // Set the SummaryActivity to launch when clicked
+        Intent appIntent = new Intent(context, SummaryActivity.class);
+        PendingIntent appPendingIntent =
+                PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setPendingIntentTemplate(R.id.widget_grid_view, appPendingIntent);
+
+        return remoteViews;
+
     }
 }
 
